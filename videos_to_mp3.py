1a700d568736a59b60a3deafd1576dcce6ddd042
Prabhat <pk2020kr@gmail.com>
2026-02-15 03:31:19 +0530

    Converts the videos to mp3

---
+++ videos_to_mp3.py
@@ -0,0 +1,9 @@
+# Converts the videos to mp3 
+import os 
+import subprocess
+
+files = os.listdir("videos")
+for file in files: 
+    file_name = file.split(".")[0]
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{file_name}.mp3"])
+    
